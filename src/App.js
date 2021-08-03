1abf628babb4feee54b98a89500fd97ef0bcfee1
max-i-mys <maximys6205@gmail.com>
2021-08-03 12:10:53 +0300

    divided the project into main components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-
+import "./App.css"
+import Graph from "./components/Graph"
+import Header from "./components/Header"
+import Ticker from "./components/Ticker"
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="App container mx-auto flex flex-col items-center bg-gray-100 p-4">
+				<div className="container">
+					<Header />
+					<hr className="w-full border-t border-gray-600 my-4" />
+					<dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+						<Ticker />
+					</dl>
+					<hr className="w-full border-t border-gray-600 my-4" />
+					<Graph />
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
